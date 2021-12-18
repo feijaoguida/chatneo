c5871effad7158e8a6266fc2a0b534e177e1cf00
Roseweltty <roseweltty@visie.com.br>
2021-12-18 18:48:15 -0300

    feat: Criação do layout default

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container fluid="xl" class="mt-5 d-flex flex-column align-items-center">
+    <p class="h4 m-5">CENTRAL DE ATENDIMENTO</p>
+
+    <Nuxt />
+
+    <div class="innerdiv">
+      <Assist />
+    </div>
+  </b-container>
+</template>
+
+<style scoped>
+.innerdiv {
+  position: absolute;
+  float: right;
+  right: 1em;
+  bottom: 0;
+  margin-bottom: 2em;
+  /* Push it up into the element before it. This is a trick for bottom-right */
+  right: 1em;
+  z-index: 200;
+}
+</style>
