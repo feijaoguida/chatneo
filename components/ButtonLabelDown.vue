84e636b1ed6e25c148a90eb064ff5a4bab68a9c3
Roseweltty <roseweltty@visie.com.br>
2021-12-18 19:14:18 -0300

    feat: Tela de Busca e componente ButtonLabelDown

---
+++ components/ButtonLabelDown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="button d-flex flex-column align-items-center">
+    <b-button @click="$emit('click')" variant="danger" class="button pt-2">
+      <b-icon font-scale="3" :icon="icon"></b-icon>
+    </b-button>
+    <div>
+      <p class="font">{{label}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: { type: String, required: false },
+    label: { type: String, required: true }
+  }
+};
+</script>
+
+<style scoped>
+.button {
+  max-width: 80px;
+  max-height: 80px;
+}
+
+.font {
+  font-size: 0.9rem;
+  color: #343a40;
+}
+</style>
---
+++ pages/telaBusca.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex flex-column container align-items-center">
+    <div class="d-flex w-75 flex-column container align-items-center">
+      <p>Como podemos te ajudar</p>
+      <b-input-group size="lg" class="mb-2">
+        <b-form-input aria-label="Large text input with switch"></b-form-input>
+        <b-input-group-append is-text>
+          <b-icon icon="search"></b-icon>
+        </b-input-group-append>
+      </b-input-group>
+      <p>Navegue pelos assuntos mais buscados</p>
+    </div>
+    <div class="d-flex flex-row w-100 justify-content-between">
+      <ButtonLabelDown @click="navegarTela" label="Cadastro" icon="person-fill" />
+      <ButtonLabelDown @click="navegarTela" label="Entregas" icon="truck" />
+      <ButtonLabelDown @click="navegarTela" label="Trocas" icon="arrow-down-up" />
+      <ButtonLabelDown @click="navegarTela" label="Pedidos" icon="bag" />
+      <ButtonLabelDown @click="navegarTela" label="Produtos" icon="lamp-fill" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navegarTela() {
+      this.$router.push(`/telaResultado`);
+    }
+  }
+};
+</script>
