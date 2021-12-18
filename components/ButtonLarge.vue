e143737f04e3b9f769cca2ceaa024d6429c040d2
Roseweltty <roseweltty@visie.com.br>
2021-12-18 19:02:35 -0300

    feat: Criação do componente Button Large

---
+++ components/ButtonLarge.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-button @click="$emit('click')" variant="danger" class="pt-2">
+    <b-icon :font-scale="scale" :icon="icon"></b-icon>
+    <div>{{description}}</div>
+  </b-button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: { type: String, required: false },
+    description: { type: String, required: true },
+    scale: { type: String, required: false }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,31 @@
 <template>
-  <b-container class="mt-5 d-flex flex-column align-items-center">
-    <p class="h4">CENTRAL DE ATENDIMENTO</p>
-    <p class="h6">
-      <b-icon icon="tools"></b-icon>Sua compra foi feita hein
-    </p>
-    <div class="w-50 d-flex justify-content-around">
-      <b-button variant="danger" class="pt-4 pb-4">
-        <b-icon icon="Phone"></b-icon>
-        <p>Loja Online</p>
-      </b-button>
-      <b-button variant="danger" class="pt-4 pb-4">
-        <b-icon icon="shop"></b-icon>
-        <p>Loja Fisica</p>
-      </b-button>
+  <div>
+    <p class="h6 align-self-center">Sua compra foi feita hein</p>
+    <div class="m-5 w-button d-flex justify-content-around">
+      <ButtonLarge @click="link" description="Loja Online" scale="3" icon="Phone" />
+      <ButtonLarge @click="linkExterno" description="Loja Física" scale="3" icon="shop" />
     </div>
-  </b-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "IndexPage"
+  name: "IndexPage",
+
+  methods: {
+    linkExterno() {
+      window.open("https://linktr.ee/rbgsolucoes", "_blank");
+    },
+
+    link() {
+      this.$router.push(`/telaBusca`);
+    }
+  }
 };
 </script>
+
+<style scoped>
+.w-button {
+  width: 300px !important;
+}
+</style>
