55d135121abce4e885ef37bdba8bef8f513837ff
Roseweltty <roseweltty@visie.com.br>
2021-12-19 21:46:35 -0300

    fix: ajuste toggle

--- pages/telaResultado/cadastroChild.vue
+++ pages/telaResultado/cadastroChild.vue
@@ -3,91 +3,41 @@
     <div>
       <h4>Cadastro</h4>
     </div>
-    <div class="w-100">
+    <div class="w-100" v-for="(item, idx) in items" :key="idx">
       <span
         class="align-items-lg-start w-100 fontSize"
-        v-b-toggle.collapse-1
-        @click="visible = !visible"
+        v-b-toggle="`${item.toggle}`"
+        @click="item.visible = !item.visible"
       >
-        <b-icon class="mr-2" font-scale="1" :icon="visible ? 'chevron-down' : 'chevron-right'"></b-icon>Como me cadastrar no site?
+        <b-icon class="mr-2" font-scale="1" :icon="item.visible ? 'chevron-down' : 'chevron-right'"></b-icon>
+        {{item.pergunta}}
       </span>
-      <b-collapse class="mt-2 ml-4" id="collapse-1">
-        <p class="fontSize">
-          Collapse contents Here Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Id possimus, illum similique
-          accusantium, nisi in nemo, nesciunt quos ea quae autem debitis
-          perferendis est vitae odit aperiam velit eveniet repellendus.
-        </p>
+      <b-collapse class="mt-2 ml-4" :id="item.toggle">
+        <p class="fontSize">{{item.texto}}</p>
         <div>
           <span class="fontSize">Essa resposta Foi util</span>
           <b-button
             class="fontSize"
             size="sm"
             squared
-            @click="resposta = 'SIM'"
+            @click="item.resposta = 'SIM'"
             variant="success"
           >Sim</b-button>
           <b-button
             class="fontSize"
             size="sm"
             squared
-            @click="resposta = 'NAO'"
+            @click="item.resposta = 'NAO'"
             variant="danger"
           >Não</b-button>
         </div>
-        <div class="mt-3" v-if="resposta == 'SIM'">
+        <div class="mt-3" v-if="item.resposta == 'SIM'">
           <p class="title fontSize">Que bom que conseguimos ajudar</p>
           <p
             class="text-muted fontSize"
           >Você mereçe o melhor atendimento e ficamos felizes em te ajudar</p>
         </div>
-        <div class="mt-3" v-if="resposta == 'NAO'">
-          <p class="title fontSize">Sentimos por não ajudar</p>
-          <p
-            class="text-muted fontSize"
-          >Você mereçe o melhor atendimento e ficamos felizes em te ajudar</p>
-        </div>
-      </b-collapse>
-    </div>
-    <div class="w-100">
-      <span
-        class="align-items-lg-start w-100 fontSize"
-        v-b-toggle.collapse-2
-        @click="visible2 = !visible2"
-      >
-        <b-icon class="mr-2" font-scale="1" :icon="visible2 ? 'chevron-down' : 'chevron-right'"></b-icon>Como recupero minha Senha?
-      </span>
-      <b-collapse class="mt-2 ml-4" id="collapse-2">
-        <p class="fontSize">
-          Collapse contents Here Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Id possimus, illum similique
-          accusantium, nisi in nemo, nesciunt quos ea quae autem debitis
-          perferendis est vitae odit aperiam velit eveniet repellendus.
-        </p>
-        <div>
-          <span class="fontSize">Essa resposta Foi util</span>
-          <b-button
-            class="fontSize"
-            size="sm"
-            squared
-            @click="resposta = 'SIM'"
-            variant="success"
-          >Sim</b-button>
-          <b-button
-            class="fontSize"
-            size="sm"
-            squared
-            @click="resposta = 'NAO'"
-            variant="danger"
-          >Não</b-button>
-        </div>
-        <div class="mt-3" v-if="resposta == 'SIM'">
-          <p class="title fontSize">Que bom que conseguimos ajudar</p>
-          <p
-            class="text-muted fontSize"
-          >Você mereçe o melhor atendimento e ficamos felizes em te ajudar</p>
-        </div>
-        <div class="mt-3" v-if="resposta == 'NAO'">
+        <div class="mt-3" v-if="item.resposta == 'NAO'">
           <p class="title fontSize">Sentimos por não ajudar</p>
           <p
             class="text-muted fontSize"
@@ -102,9 +52,32 @@
 export default {
   data() {
     return {
-      visible: false,
-      visible2: false,
-      resposta: ""
+      items: [
+        {
+          visible: false,
+          resposta: "",
+          toggle: "toggle1",
+          pergunta: "Como me cadastrar no site?",
+          texto:
+            "Collapse contents Here Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus, illum similique accusantium, nisi in nemo, nesciunt quos ea quae autem debitis perferendis est vitae odit aperiam velit eveniet repellendus."
+        },
+        {
+          visible: false,
+          resposta: "",
+          toggle: "toggle2",
+          pergunta: "Como recupero minha senha?",
+          texto:
+            "Collapse contents Here Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus, illum similique accusantium, nisi in nemo, nesciunt quos ea quae autem debitis perferendis est vitae odit aperiam velit eveniet repellendus."
+        },
+        {
+          visible: false,
+          resposta: "",
+          toggle: "toggle3",
+          pergunta: "é possível comprar no site sem me cadastrar?",
+          texto:
+            "Collapse contents Here Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus, illum similique accusantium, nisi in nemo, nesciunt quos ea quae autem debitis perferendis est vitae odit aperiam velit eveniet repellendus."
+        }
+      ]
     };
   }
 };
